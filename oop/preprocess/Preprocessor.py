758afbb9117c93cbba017a0d0a75d6ca80838404
Danny Padilla <dpadilla010@gmail.com>
2018-03-07 18:39:56 -0800

    add queue

--- oop/preprocess/Preprocessor.py
+++ oop/preprocess/Preprocessor.py
@@ -4,11 +4,19 @@ import cv2
 class Preprocessor():
 
     def __init__(self):
-        self.preprocessor_list = []
+        self.preprocessor_queue = [] # sb a queue data struct
+        self.preprocessor_output = [] # store each preprocess output
+        self.pp_list = {} # store in dictionary instead?
         self.roi = []
 
     def add_preprocess(self, preprocess):
-        self.preprocessor_list.append(preprocess)
+        self.preprocessor_queue.append(preprocess)
 
-    def get_preprocessor_list(self):
-        return self.preprocessor_list
+    def get_preprocessor_queue(self):
+        return self.preprocessor_queue
+
+    # loop and run each preprocess
+    ## SHOULD CHANGE SO EACH PREPROCESS RETURNS JUST ONE VALUE
+    def run(self):
+        for process in self.preprocessor_list:
+            self.preprocessor_output.append(process.get_output() )
